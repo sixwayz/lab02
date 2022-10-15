ab0adb1686988a80c865749d2316fe6530a55ac8
sixwayz <sixwayz.topchek@mail.ru>
2022-10-15 13:58:04 +0300

    adding 1st lw

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+just add a string for task
+
+int main()
+{
+	cout << "Enter your name:";
+	string name;
+	cin >> name;
+	cout << "Hello, world from " << name;
+	return 0;
+}
